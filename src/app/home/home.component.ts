1c2514ddff2277c48fce8f9579a95b47e8926d9e
02l <mitrakos12@gmail.com>
2017-01-27 05:54:16 -0800

    List functionality, and form reimplement

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  songToSearch: string;
+  trainName: string;
+  trainImgPath: string;
+  trainTags: string;
 
-  constructor() { }
+  public songResults = [];
+  public songToPlay = '';
+  public displayAudioTag = false;
+  public selectedTrack = {
+    song: 'Select your song below',
+    ph: ''
+  };
+  public trains = [];
+
+  constructor(public apiService: ApiService) { }
 
   ngOnInit() {
   }
 
+  searchForSong = () => {
+    this.apiService.getRelevantSongs(this.songToSearch).subscribe(res => {
+      this.songToSearch = '';
+      this.songResults = res.json();
+    }, err => {
+      console.log('err', err);
+    });
+  }
+
+  firstSongInTrain = idx => {
+    this.selectedTrack = this.songResults[idx];
+    this.selectedTrack.ph = ':';
+  }
+
+  createTrain = () => {
+    var opts = {
+      selectedTrack: this.selectedTrack,
+      name: this.trainName,
+      imgurl: this.trainImgPath,
+      tags: this.trainTags
+    }
+    console.log('opts', opts);
+    this.apiService.userSubmitsTrain(opts);
+  }
 }
